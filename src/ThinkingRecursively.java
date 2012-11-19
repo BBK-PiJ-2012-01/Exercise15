66bf37be15b9d4e3db9f7d9ca5f5adc403e28332
Sam Wright <swrigh11@dcs.bbk.ac.uk>
2012-11-19 14:20:26 +0000

    First done

---
+++ src/ThinkingRecursively.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sam Wright
+ * Date: 19/11/2012
+ * Time: 13:41
+ */
+public class ThinkingRecursively {
+    private static List<Integer> output = new LinkedList<Integer>();
+
+    public static void printNumbers(int n) {
+        if (n <= 0) {
+            return;
+        }
+        println(n);
+        printNumbers(n - 2);
+        printNumbers(n - 3);
+        println(n);
+    }
+
+    public String doggyMethod(int n) {
+        String result = doggyMethod(n - 3) + n + doggyMethod(n - 2);
+
+        // Recursion occurs before basis check!!!
+        if (n <= 0) {
+            return "";
+        }
+
+        return result;
+    }
+
+    public static String mcCarthy91(int n) {
+        if (n > 100) {
+            return "" + (n - 10);
+        } else {
+            return mcCarthy91(Integer.valueOf(mcCarthy91(n + 11)));
+        }
+    }
+
+    public static void println(int num){
+        output.add(num);
+        System.out.println(num);
+    }
+
+    public static boolean checkOutput(List<Integer> expected) {
+        return output.equals(expected);
+    }
+
+    public static void clearOutput() {
+        output.clear();
+    }
+}
---
+++ src/ThinkingRecursivelyTest.java
@@ -0,0 +1,62 @@
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+* Created with IntelliJ IDEA.
+* User: Sam Wright
+* Date: 19/11/2012
+* Time: 13:50
+*/
+public class ThinkingRecursivelyTest {
+    List<Integer> expected;
+
+    private void setExpected(Integer[] expected_array) {
+        expected = new ArrayList<Integer>(Arrays.asList(expected_array));
+        ThinkingRecursively.clearOutput();
+    }
+
+    @Test
+    public void testPrintNumbers() throws Exception {
+        setExpected(new Integer[]{6,4,2,2,1,1,4,3,1,1,3,6});
+        ThinkingRecursively.printNumbers(6);
+        assertTrue(ThinkingRecursively.checkOutput(expected));
+    }
+
+    @Test
+    public void testDoggyMethod() throws Exception {
+
+    }
+
+    @Test
+    public void testMcCarthy91() throws Exception {
+        assertEquals((Integer) 91, Integer.valueOf(ThinkingRecursively.mcCarthy91(50)));
+        assertEquals((Integer) 91, Integer.valueOf(ThinkingRecursively.mcCarthy91(73)));
+        assertEquals((Integer) 91, Integer.valueOf(ThinkingRecursively.mcCarthy91(95)));
+    }
+
+    @Test
+    public void testCheckOutput() throws Exception {
+        setExpected(new Integer[]{3,4,5});
+
+        ThinkingRecursively.println(3);
+        ThinkingRecursively.println(4);
+        ThinkingRecursively.println(5);
+
+        assertTrue(ThinkingRecursively.checkOutput(expected));
+
+        setExpected(new Integer[]{1,2,3});
+
+        ThinkingRecursively.println(1);
+        ThinkingRecursively.println(2);
+        ThinkingRecursively.println(3);
+
+        assertTrue(ThinkingRecursively.checkOutput(expected));
+
+    }
+}
